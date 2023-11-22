5338f72d1ede48e1239e0766fdf5ea6ee8b1bc70
isamaelfarah <121323034+isamaelfarah@users.noreply.github.com>
2023-11-22 09:32:35 -0500

    Create حساب العمر.c

---
+++ حساب العمر.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+ int age,year,c;
+printf("inter you berthday: ");
+scanf("%d",&year);
+printf("inter the current year:  ");
+scanf("%d",&c);
+age = c - year;
+printf("your age is : %d",age);
+  return 0;
+}
